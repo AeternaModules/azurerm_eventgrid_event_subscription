ec282d53adfd7ca10da7256c3dc4e6f3069a5af3
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:51:15 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -111,8 +111,8 @@ EOT
   type = map(object({
     name                                 = string
     scope                                = string
-    advanced_filtering_on_arrays_enabled = optional(bool, false)
-    event_delivery_schema                = optional(string, "EventGridSchema")
+    advanced_filtering_on_arrays_enabled = optional(bool)   # Default: false
+    event_delivery_schema                = optional(string) # Default: "EventGridSchema"
     eventhub_endpoint_id                 = optional(string)
     expiration_time_utc                  = optional(string)
     hybrid_connection_endpoint_id        = optional(string)
